c2338ba95416e521b74a78bfde828878f91ef0eb
linmaoxin <linmaoxin@gomo.com>
2022-09-05 23:53:08 +0800

    blend sample

--- Android/PlayerProj/app/src/main/java/com/tencent/qgame/playerproj/SimpleFaceDetectActivity.kt
+++ Android/PlayerProj/app/src/main/java/com/tencent/qgame/playerproj/SimpleFaceDetectActivity.kt
@@ -1,25 +1,26 @@
 package com.tencent.qgame.playerproj
 
-import android.content.res.AssetManager
 import android.graphics.*
+import android.os.Build.VERSION_CODES.S
 import android.os.Bundle
+import android.util.Size
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.minus
 import androidx.lifecycle.lifecycleScope
 import com.at.lottie.utils.logd
 import com.at.lottie.utils.loge
-import com.blankj.utilcode.util.Utils
+import com.at.lottie.utils.logi
 import com.google.android.gms.tasks.Tasks
-import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetector
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import com.tencent.qgame.playerproj.databinding.ActivitySimpleFaceDetectBinding
-import com.tencent.qgame.playerproj.databinding.ActivitySimpleLottieBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 /**
  * Created by linmaoxin on 2022/9/2
@@ -42,25 +43,84 @@ class SimpleFaceDetectActivity : AppCompatActivity() {
                     val bmp = resources.assets.open("lottie_aging/images/img_0.png").use { ips ->
                         BitmapFactory.decodeStream(ips, null, BitmapFactory.Options().also { it.inMutable = true })
                     }
-                    Tasks.await(detector.process(bmp!!, 0)).getOrNull(0)?.let { face ->
+                    val index = Random.nextInt(2)
+                    val dstBmp = resources.assets.open("aging/img_${index}.png").use { ips->
+                        BitmapFactory.decodeStream(ips, null, BitmapFactory.Options().also { it.inMutable = true })
+                    }
+                    val srcFace = Tasks.await(detector.process(bmp!!, 0)).getOrNull(0)
+                    val dstFace = Tasks.await(detector.process(dstBmp!!, 0)).getOrNull(0)
+                    takeIf { srcFace!= null && dstFace != null }?.let {
+                        srcFace!!;dstFace!!
                         val w = bmp.width.toFloat()
                         val h = bmp.height.toFloat()
-                        val box = face.boundingBox
-                        loge("@@@", "face:${box}, ${box.centerX()} - ${box.centerY()}")
-                        loge("@@@", "l:${(box.left / w).format()}, t:${(box.top / h).format()}, r:${(box.right / w).format()}, b:${(box.bottom / h).format()}")
+                        val srcBox = srcFace.boundingBox
+                        val dstBox = dstFace.boundingBox
+                        loge("@@@", "face:${srcBox}, ${srcBox.centerX()} - ${srcBox.centerY()}")
+                        loge("@@@", "l:${(srcBox.left / w).format()}, t:${(srcBox.top / h).format()}, r:${(srcBox.right / w).format()}, b:${(srcBox.bottom / h).format()}")
+
                         Canvas(bmp).also { cv ->
-                            val path = ptsFToPath(Array(4) { PointF() }.also { box.setPts(it) })
+                            Canvas(dstBmp).also { dstcv ->
+                                val paint = Paint().apply {
+                                    color = Color.YELLOW
+                                    style = Paint.Style.STROKE
+                                    strokeWidth = 2f
+                                }
+                                val frame = ptsFToPath(Array(4) { PointF() }.also {
+                                    it[0].set(dstBox.left.toFloat() - 2, dstBox.top.toFloat() - 2)
+                                    it[1].set(dstBox.right.toFloat() + 2, dstBox.top.toFloat() - 2)
+                                    it[2].set(dstBox.right.toFloat() + 2, dstBox.bottom.toFloat() + 2)
+                                    it[3].set(dstBox.left.toFloat() - 2, dstBox.bottom.toFloat() + 2)
+                                })
+                                dstcv.drawPath(frame, paint)
+                                paint.apply {
+                                    color = Color.YELLOW
+                                    style = Paint.Style.FILL
+                                    strokeWidth = 2f
+                                }
+                                dstcv.drawCircle(dstBox.centerX().toFloat(), dstBox.centerY().toFloat(), 12f, paint)
+                            }
+
+                            var dstF = if (srcBox.height() > dstBox.height()) {
+                                getPuzzleOutSize(dstBox.width(), dstBox.height(), srcBox.height())
+                            } else {
+                                Size(dstBox.width(), dstBox.height())
+                            }
+                            var s: Float = (dstF.height / dstBox.height().toFloat())
+
+                            //todo
+                            val newDstSize = Size((dstBmp.width * s).toInt(), (dstBmp.height * s).toInt())
+                            // if (newDstSize.height > h) {
+                            //     s = h / dstBmp.height
+                            //     dstF = getPuzzleOutSize(dstBox.width(), dstBox.height(), h.toInt())
+                            // }
+
+                            val x = (srcBox.centerX() - (dstBox.centerX() * s))
+                            val y = (srcBox.centerY() - (dstBox.centerY() * s))
+                            val m = Matrix()
+                            m.postScale(s, s)
+                            m.postTranslate(x,y)
+                            cv.drawBitmap(dstBmp, m, null)
+
+                            val path = ptsFToPath(Array(4) { PointF() }.also { srcBox.setPts(it) })
                             val paint = Paint().apply {
                                 color = Color.RED
                                 style = Paint.Style.FILL
                                 strokeWidth = 3f
                             }
-                            cv.drawCircle(face.boundingBox.centerX().toFloat(), face.boundingBox.centerY().toFloat(), 9f, paint)
+                            cv.drawCircle(srcFace.boundingBox.centerX().toFloat(), srcFace.boundingBox.centerY().toFloat(), 9f, paint)
                             paint.apply {
                                 style = Paint.Style.STROKE
                                 strokeWidth = 2f
                             }
                             cv.drawPath(path, paint)
+
+                            val frame = ptsFToPath(Array(4) { PointF() }.also {
+                                it[0].set(0f, 0f)
+                                it[1].set(bmp.width.toFloat(), 0f)
+                                it[2].set(bmp.width.toFloat(), bmp.height.toFloat())
+                                it[3].set(0f, bmp.height.toFloat())
+                            })
+                            cv.drawPath(frame, paint)
                         }
                         emit(bmp)
                     } ?: emit(null)
@@ -73,6 +133,15 @@ class SimpleFaceDetectActivity : AppCompatActivity() {
 
         }
     }
+    fun getPuzzleOutSize(previewW: Int, previewH: Int, maxSize: Int): Size {
+        val ratio = previewW.toFloat() / previewH
+        return if (ratio < 1) {
+            Size((maxSize * ratio).toInt(), maxSize)
+        } else {
+            Size(maxSize, (maxSize / ratio).toInt())
+        }
+    }
+
 
     fun Float.format() = String.format("%.2f", this)
     fun ptsFToPath(pts: Array<PointF>): Path {
